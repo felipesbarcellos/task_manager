aa25ecb2583a2e7b66f39b66282de4446345cc03
Felipe Barcellos <felipe.cellos@hotmail.com>
2023-09-17 02:21:23 -0300

    creating add task view

--- views/addtask.py
+++ views/addtask.py
@@ -1,5 +1,10 @@
+from utils.constants import *
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
+
+Window.clearcolor = get_color_from_hex(WINDOW_COLOR)
 
 class TaskLayout(BoxLayout):
     ...
