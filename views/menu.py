f002cb1cc0e6be70a58376e1362f2ae6f146986c
Felipe Barcellos <felipe.cellos@hotmail.com>
2023-09-17 02:22:20 -0300

    trying some responsivity on menu view

--- views/menu.py
+++ views/menu.py
@@ -1,9 +1,10 @@
+from utils.constants import *
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
 
-Window.clearcolor = get_color_from_hex("#05050c")
+Window.clearcolor = get_color_from_hex(WINDOW_COLOR)
 
 class Menu(BoxLayout):
     ...
